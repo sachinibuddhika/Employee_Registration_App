811df9288598288019a55f763b77313eaeadbbf4
Sachini Buddhika <sachiniste3@gmail.com>
2024-10-20 16:11:48 +0530

    first commit-added basics setup for backend

---
+++ crud/src/main/java/com/youtube/crud/controller/EmployeeController.java
@@ -0,0 +1,8 @@
+package com.youtube.crud.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+}
---
+++ crud/src/main/java/com/youtube/crud/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.youtube.crud.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+}
