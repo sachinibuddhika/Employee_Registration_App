bc851a796301032c0f529fb936d10b6c7b67ff4b
Sachini Buddhika <sachiniste3@gmail.com>
2024-10-21 14:12:45 +0530

    Created APIs for add,update and delete employee

--- crud/src/main/java/com/youtube/crud/controller/EmployeeController.java
+++ crud/src/main/java/com/youtube/crud/controller/EmployeeController.java
@@ -1,8 +1,44 @@
 package com.youtube.crud.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.youtube.crud.entity.Employee;
+import com.youtube.crud.service.EmployeeService;
+import org.hibernate.id.IntegralDataTypeHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
+
+@Autowired
+private EmployeeService employeeService;
+    @PostMapping("/save/employee")
+    public Employee saveEmployee(@RequestBody Employee employee){
+    return employeeService.saveEmployee(employee);
+    }
+    @GetMapping("/get/employees")
+    public List<Employee> getEmployees(){
+     return employeeService.getEmployees();
+    }
+
+    @GetMapping("get/employee/{employeeId}")
+    public Employee getEmployeeById(@PathVariable Integer employeeId){
+     return employeeService.getEmployeeById(employeeId);
+    }
+
+@DeleteMapping("/delete/employee/{employeeId}")
+    public void deleteEmployee(@PathVariable Integer employeeId){
+
+        employeeService.deleteEmployee(employeeId);
+
+}
+
+
+@PutMapping("/update/employee")
+public Employee updateEmployee(@RequestBody Employee employee){
+return employeeService.updateEmployee(employee);
+}
+
 }
