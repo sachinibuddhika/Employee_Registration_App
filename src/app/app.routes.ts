6945ad85fb830a5743db532418e5e74906382ce2
Sachini Buddhika <sachiniste3@gmail.com>
2024-10-27 12:18:59 +0530

    created home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'header',component:HeaderComponent},
+    {path:'',component:HomeComponent}
+   
+];
